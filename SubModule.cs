68cd365c084d5c9c1d487d01d56ec2988c636a97
Nico Welles <nico@welles.io>
2020-04-20 20:35:16 +0200

    Cleanup SubModule.cs

--- SubModule.cs
+++ SubModule.cs
@@ -5,30 +5,6 @@ namespace BannerlordCheats
 {
     public class SubModule : MBSubModuleBase
     {
-        /*protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
-        {
-            base.OnGameStart(game, gameStarterObject);
-
-            if (gameStarterObject is CampaignGameStarter campaignGameStarter)
-            {
-                campaignGameStarter.AddModel(new CheatClanTierModel());
-                campaignGameStarter.AddModel(new CheatPartySizeLimitModel());
-                campaignGameStarter.AddModel(new CheatPartySpeedCalculatingModel());
-                campaignGameStarter.AddModel(new CheatSiegeEventModel());
-                campaignGameStarter.AddModel(new CheatAgentApplyDamageModel());
-                campaignGameStarter.AddModel(new CheatGenericXpModel());
-                campaignGameStarter.AddModel(new CheatInventoryCapacityModel());
-                campaignGameStarter.AddModel(new CheatHeroDeathProbabilityCalculationModel());
-                campaignGameStarter.AddModel(new CheatPartyMoraleModel());
-                campaignGameStarter.AddModel(new CheatCharacterDevelopmentModel());
-                campaignGameStarter.AddModel(new CheatMapVisibilityModel());
-                campaignGameStarter.AddModel(new CheatSmithingModel());
-                campaignGameStarter.AddModel(new CheatBuildingConstructionModel());
-                campaignGameStarter.AddModel(new CheatBattleRewardModel());
-                campaignGameStarter.AddModel(new CheatTroopCountLimitModel());
-            }
-        }*/
-
         protected override void OnSubModuleLoad()
         {
             base.OnSubModuleLoad();
