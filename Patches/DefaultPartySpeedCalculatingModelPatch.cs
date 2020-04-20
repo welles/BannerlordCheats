69437857b94df8736685c20250fb09faa176997b
Nico Welles <nico@welles.io>
2020-04-20 19:07:22 +0200

    Move party speed cheat to patch class

---
+++ Patches/DefaultPartySpeedCalculatingModelPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
+
+namespace BannerlordCheats.Patches
+{
+    [HarmonyPatch(typeof(DefaultPartySpeedCalculatingModel), "CalculateFinalSpeed")]
+    public class DefaultPartySpeedCalculatingModelPatch
+    {
+        [HarmonyPostfix]
+        public static void CalculateFinalSpeed(MobileParty mobileParty, float baseSpeed, StatExplainer explanation, ref float __result)
+        {
+            if (mobileParty?.IsMainParty ?? false)
+            {
+                __result = __result * 4;
+            }
+        }
+    }
+}
