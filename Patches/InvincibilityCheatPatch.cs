6cece734484d5867d5d20799fb01c63cf192da45
Nico Welles <nico@welles.io>
2020-04-30 14:29:25 +0200

    Rename invincibility cheat class

--- Patches/InvincibilityCheatPatch.cs
+++ Patches/InvincibilityCheatPatch.cs
@@ -7,7 +7,7 @@ using TaleWorlds.MountAndBlade;
 namespace BannerlordCheats.Patches
 {
     [HarmonyPatch(typeof(SandboxAgentApplyDamageModel), "CalculateDamage")]
-    public static class SandboxAgentApplyDamageModelPatch
+    public static class InvincibilityCheatPatch
     {
         [HarmonyPostfix]
         public static void CalculateDamage(BasicCharacterObject affectorBasicCharacter, BasicCharacterObject affectedBasicCharacter, MissionWeapon offHandItem, bool isHeadShot, bool isAffectedAgentMount, bool isAffectedAgentHuman, bool hasAffectorAgentMount, bool isAffectedAgentNull, bool isAffectorAgentHuman, AttackCollisionData collisionData, WeaponComponentData weapon, ref int __result)
