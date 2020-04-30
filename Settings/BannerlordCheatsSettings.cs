f655c59638edb4e0dc0a01dcf30dc905857473c4
Nico Welles <nico@welles.io>
2020-04-30 15:36:56 +0200

    Update MBOptionsScreen to 2.0.10

--- Settings/BannerlordCheatsSettings.cs
+++ Settings/BannerlordCheatsSettings.cs
@@ -1,4 +1,5 @@
 ﻿using MBOptionScreen.Attributes;
+using MBOptionScreen.Attributes.v2;
 using MBOptionScreen.Settings;
 
 namespace BannerlordCheats.Settings
@@ -20,7 +21,7 @@ namespace BannerlordCheats.Settings
         #region Hotkeys
 
         [SettingPropertyGroup("Hotkeys")]
-        [SettingProperty(displayName: "Enable Hotkeys", requireRestart: false, hintText: "Enable cheat hotkeys in inventory screen, party screen, clan screen etc.")]
+        [SettingPropertyBool("Enable Hotkeys", RequireRestart = false, HintText = "Enable cheat hotkeys in inventory screen, party screen, clan screen etc.")]
         public bool EnableHotkeys { get; set; } = false;
 
         #endregion Hotkeys
@@ -28,11 +29,11 @@ namespace BannerlordCheats.Settings
         #region Map
 
         [SettingPropertyGroup("Map")]
-        [SettingProperty(displayName: "Map Speed Factor", minValue: 1f, maxValue: 100f, editableMinValue: 1f, editableMaxValue: 100f, requireRestart: false, hintText: "Factor by which the speed of the player on the map is multiplied. A factor of 1 means default speed.")]
+        [SettingPropertyFloatingInteger(displayName: "Map Speed Factor", minValue: 1f, maxValue: 100f, RequireRestart = false, HintText = "Factor by which the speed of the player on the map is multiplied. A factor of 1 means default speed.")]
         public float MapSpeedFactor { get; set; } = 1.0f;
 
         [SettingPropertyGroup("Map")]
-        [SettingProperty(displayName: "Map Visibility Factor", minValue: 1f, maxValue: 10f, editableMinValue: 1f, editableMaxValue: 10f, requireRestart: false, hintText: "Factor by which the visibility range of the player on the map is multiplied. A factor of 1 means default visibility. WARNING: Will negatively affect performance!")]
+        [SettingPropertyFloatingInteger(displayName: "Map Visibility Factor", minValue: 1f, maxValue: 10f, RequireRestart = false, HintText = "Factor by which the visibility range of the player on the map is multiplied. A factor of 1 means default visibility. WARNING: Will negatively affect performance!")]
         public float MapVisibilityFactor { get; set; } = 1.0f;
 
         #endregion Map
@@ -40,19 +41,19 @@ namespace BannerlordCheats.Settings
         #region Combat
 
         [SettingPropertyGroup("Combat")]
-        [SettingProperty(displayName: "Invincible", requireRestart: false, hintText: "Disables all damage to the player character.")]
+        [SettingPropertyBool(displayName: "Invincible", RequireRestart = false, HintText = "Disables all damage to the player character.")]
         public bool Invincible { get; set; } = false;
 
         [SettingPropertyGroup("Combat")]
-        [SettingProperty(displayName: "One Hit Kill", requireRestart: false, hintText: "Kill enemies with one hit.")]
+        [SettingPropertyBool(displayName: "One Hit Kill", RequireRestart = false, HintText = "Kill enemies with one hit.")]
         public bool OneHitKill { get; set; } = false;
 
         [SettingPropertyGroup("Combat")]
-        [SettingProperty(displayName: "Renown Reward Multiplier", minValue: 1f, maxValue: 1000f, editableMinValue: 1f, editableMaxValue: 1000f, requireRestart: false, hintText: "Factor by which the renown reward after a won battle is multiplied. A factor of 1 means default renown.")]
+        [SettingPropertyFloatingInteger(displayName: "Renown Reward Multiplier", minValue: 1f, maxValue: 1000f, RequireRestart = false, HintText = "Factor by which the renown reward after a won battle is multiplied. A factor of 1 means default renown.")]
         public float RenownRewardMultiplier { get; set; } = 1.0f;
 
         [SettingPropertyGroup("Combat")]
-        [SettingProperty(displayName: "Influence Reward Multiplier", minValue: 1f, maxValue: 1000f, editableMinValue: 1f, editableMaxValue: 1000f, requireRestart: false, hintText: "Factor by which the influence reward after a won battle is multiplied. A factor of 1 means default influence.")]
+        [SettingPropertyFloatingInteger(displayName: "Influence Reward Multiplier", minValue: 1f, maxValue: 1000f, RequireRestart = false, HintText = "Factor by which the influence reward after a won battle is multiplied. A factor of 1 means default influence.")]
         public float InfluenceRewardMultiplier { get; set; } = 1.0f;
 
         #endregion Combat
@@ -60,7 +61,7 @@ namespace BannerlordCheats.Settings
         #region Inventory
 
         [SettingPropertyGroup("Inventory")]
-        [SettingProperty(displayName: "Extra Inventory Capacity", minValue: 0, maxValue: 1000000, editableMinValue: 0, editableMaxValue: 1000000, requireRestart: false, hintText: "Extra inventory capacity for the player party.")]
+        [SettingPropertyInteger(displayName: "Extra Inventory Capacity", minValue: 0, maxValue: 1000000, RequireRestart = false, HintText = "Extra inventory capacity for the player party.")]
         public int ExtraInventoryCapacity { get; set; } = 0;
 
         #endregion Inventory
@@ -68,15 +69,15 @@ namespace BannerlordCheats.Settings
         #region Party
 
         [SettingPropertyGroup("Party")]
-        [SettingProperty(displayName: "Extra Party Member Size", minValue: 0, maxValue: 10000, editableMinValue: 0, editableMaxValue: 10000, requireRestart: false, hintText: "Increase the maximum size for the player party.")]
+        [SettingPropertyInteger(displayName: "Extra Party Member Size", minValue: 0, maxValue: 10000, RequireRestart = false, HintText = "Increase the maximum size for the player party.")]
         public int ExtraPartyMemberSize { get; set; } = 0;
 
         [SettingPropertyGroup("Party")]
-        [SettingProperty(displayName: "Extra Party Prisoner Size", minValue: 0, maxValue: 10000, editableMinValue: 0, editableMaxValue: 10000, requireRestart: false, hintText: "Increase the maximum amount of prisoners travelling with the party.")]
+        [SettingPropertyInteger(displayName: "Extra Party Prisoner Size", minValue: 0, maxValue: 10000, RequireRestart = false, HintText = "Increase the maximum amount of prisoners travelling with the party.")]
         public int ExtraPartyPrisonerSize { get; set; } = 0;
 
         [SettingPropertyGroup("Party")]
-        [SettingProperty(displayName: "Extra Party Morale", minValue: 0, maxValue: 100, editableMinValue: 0, editableMaxValue: 100, requireRestart: false, hintText: "Increase morale of the player party.")]
+        [SettingPropertyInteger(displayName: "Extra Party Morale", minValue: 0, maxValue: 100, RequireRestart = false, HintText = "Increase morale of the player party.")]
         public int ExtraPartyMorale { get; set; } = 0;
 
         #endregion
@@ -84,11 +85,11 @@ namespace BannerlordCheats.Settings
         #region Clan
 
         [SettingPropertyGroup("Clan")]
-        [SettingProperty(displayName: "Extra Companion Limit", minValue: 0, maxValue: 100, editableMinValue: 0, editableMaxValue: 100, requireRestart: false, hintText: "Increase the maximum amount of companions in your clan.")]
+        [SettingPropertyInteger(displayName: "Extra Companion Limit", minValue: 0, maxValue: 100, RequireRestart = false, HintText = "Increase the maximum amount of companions in your clan.")]
         public int ExtraCompanionLimit { get; set; } = 0;
 
         [SettingPropertyGroup("Clan")]
-        [SettingProperty(displayName: "Extra Clan Party Limit", minValue: 0, maxValue: 100, editableMinValue: 0, editableMaxValue: 100, requireRestart: false, hintText: "Increase the maximum amount of parties in your clan.")]
+        [SettingPropertyInteger(displayName: "Extra Clan Party Limit", minValue: 0, maxValue: 100, RequireRestart = false, HintText = "Increase the maximum amount of parties in your clan.")]
         public int ExtraClanPartyLimit { get; set; } = 0;
 
         #endregion Clan
@@ -96,11 +97,11 @@ namespace BannerlordCheats.Settings
         #region Experience
 
         [SettingPropertyGroup("Experience")]
-        [SettingProperty(displayName: "Experience Multiplier", minValue: 1, maxValue: 10000, editableMinValue: 1, editableMaxValue: 10000, requireRestart: false, hintText: "Factor by which the experience gain of the player is multiplied. A factor of 1 means default experience.")]
+        [SettingPropertyFloatingInteger(displayName: "Experience Multiplier", minValue: 1, maxValue: 10000, RequireRestart = false, HintText = "Factor by which the experience gain of the player is multiplied. A factor of 1 means default experience.")]
         public float ExperienceMultiplier { get; set; } = 1.0f;
 
         [SettingPropertyGroup("Experience")]
-        [SettingProperty(displayName: "Learning Rate Multiplier", minValue: 1, maxValue: 10000, editableMinValue: 1, editableMaxValue: 10000, requireRestart: false, hintText: "Factor by which the learning rate of the player is multiplied. A factor of 1 means default learning.")]
+        [SettingPropertyFloatingInteger(displayName: "Learning Rate Multiplier", minValue: 1, maxValue: 10000, RequireRestart = false, HintText = "Factor by which the learning rate of the player is multiplied. A factor of 1 means default learning.")]
         public float LearningRateMultiplier { get; set; } = 1.0f;
 
         #endregion Experience
@@ -108,7 +109,7 @@ namespace BannerlordCheats.Settings
         #region Sieges
 
         [SettingPropertyGroup("Sieges")]
-        [SettingProperty(displayName: "Siege Building Speed Multiplier", minValue: 1f, maxValue: 100f, editableMinValue: 1f, editableMaxValue: 100f, requireRestart: false, hintText: "Factor by which the speed of siege engine construction on the player party's side is multiplied. A factor of 1 means default build speed.")]
+        [SettingPropertyFloatingInteger(displayName: "Siege Building Speed Multiplier", minValue: 1f, maxValue: 100f, RequireRestart = false, HintText = "Factor by which the speed of siege engine construction on the player party's side is multiplied. A factor of 1 means default build speed.")]
         public float SiegeBuildingSpeedMultiplier { get; set; } = 1.0f;
 
         #endregion Sieges
@@ -116,7 +117,7 @@ namespace BannerlordCheats.Settings
         #region Fiefs
 
         [SettingPropertyGroup("Fiefs")]
-        [SettingProperty(displayName: "Construction Power Multiplier", minValue: 1, maxValue: 1000, editableMinValue: 1, editableMaxValue: 1000, requireRestart: false, hintText: "Factor by which the 'construction power' of player owned fiefs is multiplied. A factor of 1 means default construction power.")]
+        [SettingPropertyFloatingInteger(displayName: "Construction Power Multiplier", minValue: 1, maxValue: 1000, RequireRestart = false, HintText = "Factor by which the 'construction power' of player owned fiefs is multiplied. A factor of 1 means default construction power.")]
         public int ConstructionPowerMultiplier { get; set; } = 1;
 
         #endregion Fiefs
@@ -124,7 +125,7 @@ namespace BannerlordCheats.Settings
         #region Smithing
 
         [SettingPropertyGroup("Smithing")]
-        [SettingProperty(displayName: "No Energy Cost", requireRestart: false, hintText: "Disable energy cost for smithing, smelting or refining.")]
+        [SettingPropertyBool(displayName: "No Energy Cost", RequireRestart = false, HintText = "Disable energy cost for smithing, smelting or refining.")]
         public bool NoSmithingEnergyCost { get; set; } = false;
 
         #endregion Smithing
