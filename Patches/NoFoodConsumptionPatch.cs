56f4131ea7837333cd431cbe2a2ca1c98c1fb425
Nico Welles <nico@welles.io>
2020-06-20 13:07:23 +0200

    Improve No Food Consumption cheat display in UI

--- Patches/NoFoodConsumptionPatch.cs
+++ Patches/NoFoodConsumptionPatch.cs
@@ -5,16 +5,35 @@ using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
 
 namespace BannerlordCheats.Patches
 {
-    [HarmonyPatch(typeof(DefaultMobilePartyFoodConsumptionModel), "DoesPartyConsumeFood")]
+    [HarmonyPatch(typeof(DefaultMobilePartyFoodConsumptionModel), nameof(DefaultMobilePartyFoodConsumptionModel.CalculateDailyFoodConsumptionf))]
     public static class NoFoodConsumptionPatch
     {
         [HarmonyPostfix]
-        public static void DoesPartyConsumeFood(MobileParty mobileParty, ref bool __result)
+        public static void CalculateDailyFoodConsumptionf(MobileParty party, StatExplainer explainer, ref float __result)
         {
-            if ((mobileParty?.IsMainParty ?? false)
+            if ((party?.IsMainParty ?? false)
                 && BannerlordCheatsSettings.Instance.NoFoodConsumption)
             {
-                __result = false;
+                __result = 0.0f;
+
+                if (explainer != null)
+                {
+                    explainer.Lines.Clear();
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(MobileParty), nameof(MobileParty.GetNumDaysForFoodToLast))]
+    public static class NoFoodConsumptionPatchText
+    {
+        [HarmonyPostfix]
+        public static void GetNumDaysForFoodToLast(ref MobileParty __instance, ref int __result)
+        {
+            if (__instance?.IsMainParty ?? false
+                && BannerlordCheatsSettings.Instance.NoFoodConsumption)
+            {
+                __result = 1;
             }
         }
     }
