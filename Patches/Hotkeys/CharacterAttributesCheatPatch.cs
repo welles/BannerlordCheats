98d8a46270e916ae77d11d75aebc6f1ba17138f1
Nico Welles <nico@welles.io>
2020-06-21 14:18:27 +0200

    Add hotkeys to increase single attributes

--- Patches/Hotkeys/CharacterAttributesCheatPatch.cs
+++ Patches/Hotkeys/CharacterAttributesCheatPatch.cs
@@ -2,6 +2,7 @@
 using BannerlordCheats.Settings;
 using HarmonyLib;
 using SandBox.GauntletUI;
+using System;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.ViewModelCollection;
 using TaleWorlds.Core;
@@ -33,6 +34,51 @@ namespace BannerlordCheats.Patches
 
                 InformationManager.DisplayMessage(new InformationMessage($"Set attributes of {Hero.MainHero.Name} to 10.", Color.White));
             }
+
+            if (ScreenManager.TopScreen is GauntletCharacterDeveloperScreen && Keys.IsKeyPressed(InputKey.LeftControl, InputKey.D1) && BannerlordCheatsSettings.Instance.EnableHotkeys)
+            {
+                AddPoint(CharacterAttributesEnum.Vigor);
+            }
+
+            if (ScreenManager.TopScreen is GauntletCharacterDeveloperScreen && Keys.IsKeyPressed(InputKey.LeftControl, InputKey.D2) && BannerlordCheatsSettings.Instance.EnableHotkeys)
+            {
+                AddPoint(CharacterAttributesEnum.Control);
+            }
+
+            if (ScreenManager.TopScreen is GauntletCharacterDeveloperScreen && Keys.IsKeyPressed(InputKey.LeftControl, InputKey.D3) && BannerlordCheatsSettings.Instance.EnableHotkeys)
+            {
+                AddPoint(CharacterAttributesEnum.Endurance);
+            }
+
+            if (ScreenManager.TopScreen is GauntletCharacterDeveloperScreen && Keys.IsKeyPressed(InputKey.LeftControl, InputKey.D4) && BannerlordCheatsSettings.Instance.EnableHotkeys)
+            {
+                AddPoint(CharacterAttributesEnum.Cunning);
+            }
+
+            if (ScreenManager.TopScreen is GauntletCharacterDeveloperScreen && Keys.IsKeyPressed(InputKey.LeftControl, InputKey.D5) && BannerlordCheatsSettings.Instance.EnableHotkeys)
+            {
+                AddPoint(CharacterAttributesEnum.Social);
+            }
+
+            if (ScreenManager.TopScreen is GauntletCharacterDeveloperScreen && Keys.IsKeyPressed(InputKey.LeftControl, InputKey.D6) && BannerlordCheatsSettings.Instance.EnableHotkeys)
+            {
+                AddPoint(CharacterAttributesEnum.Intelligence);
+            }
+        }
+
+        private static void AddPoint(CharacterAttributesEnum type)
+        {
+            var oldValue = Hero.MainHero.GetAttributeValue(type);
+
+            if (oldValue >= 10) { return; }
+
+            Hero.MainHero.SetAttributeValue(type, oldValue + 1);
+
+            var charVM = ScreenManager.TopScreen.GetViewModel<CharacterDeveloperVM>();
+
+            charVM.RefreshValues();
+
+            InformationManager.DisplayMessage(new InformationMessage($"Added 1 point to {Enum.GetName(typeof(CharacterAttributesEnum), type)}.", Color.White));
         }
     }
 }
