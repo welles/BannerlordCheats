7ed85afd4445751c63aabc366ab1f5f02b685ec4
Nico Welles <nico@welles.io>
2020-08-29 15:38:14 +0200

    Show message that mod still works with patching error

--- MBInitialScreenBasePatch.cs
+++ MBInitialScreenBasePatch.cs
@@ -16,6 +16,7 @@ namespace BannerlordCheats
                 InformationManager.DisplayMessage(new InformationMessage("Bannerlord Cheats could not load correctly!", Color.FromUint(16711680)));
                 InformationManager.DisplayMessage(new InformationMessage($"Detailed error info has been saved in the file {SubModule.ErrorFile} in the Cheats mod folder.", Color.FromUint(16711680)));
                 InformationManager.DisplayMessage(new InformationMessage("If possible please send this file to the mod author.", Color.FromUint(16711680)));
+                InformationManager.DisplayMessage(new InformationMessage("You can still play the game, but some cheats may not work.", Color.FromUint(16711680)));
             }
         }
     }
