239367c8e3826fba5d8a5b8abdccdedc6512a6fb
Nico Welles <nico@welles.io>
2021-01-01 19:31:31 +0100

    Fix Update Instant Prisoner Recruitment cheat

--- Patches/Party/RecruitPrisonersPatch.cs
+++ Patches/Party/RecruitPrisonersPatch.cs
@@ -1,18 +1,18 @@
 ﻿using BannerlordCheats.Settings;
 using HarmonyLib;
 using TaleWorlds.CampaignSystem;
-using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
 
 namespace BannerlordCheats.Patches
 {
-    [HarmonyPatch(typeof(DefaultPrisonerRecruitmentCalculationModel), nameof(DefaultPrisonerRecruitmentCalculationModel.CalculateRecruitableNumber))]
+    [HarmonyPatch(typeof(RecruitPrisonersCampaignBehavior), nameof(RecruitPrisonersCampaignBehavior.GetRecruitableNumber))]
     public static class RecruitPrisonersPatch
     {
         [HarmonyPostfix]
-        public static void  CalculateRecruitableNumber(ref PartyBase party, ref CharacterObject character, ref int __result)
+        public static void GetRecruitableNumber(CharacterObject character, ref int __result)
         {
-            if ((party?.MobileParty?.IsMainParty ?? false)
-                && (!character?.IsHero ?? false)
+            if (character != null
+                && !character.IsHero
                 && BannerlordCheatsSettings.Instance.InstantPrisonerRecruitment)
             {
                 __result = MobileParty.MainParty.PrisonRoster.GetTroopCount(character);
