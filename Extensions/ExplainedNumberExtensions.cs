dfb7e50dc290e8ce8746da899b7794f41e1374c6
Nico Welles <nico@welles.io>
2021-03-01 02:41:18 +0100

    Add ExplainedNumber extension methods

---
+++ Extensions/ExplainedNumberExtensions.cs
@@ -0,0 +1,19 @@
+﻿using TaleWorlds.CampaignSystem;
+
+namespace BannerlordCheats.Extensions
+{
+    public static class ExplainedNumberExtensions
+    {
+        public static void AddMultiplier(this ref ExplainedNumber explainedNumber, float multiplier)
+        {
+            explainedNumber.AddFactor(multiplier - 1);
+        }
+
+        public static void AddPercentage(this ref ExplainedNumber explainedNumber, int percentage)
+        {
+            var factor = (1f - (percentage / 100f)) * -1;
+
+            explainedNumber.AddFactor(factor);
+        }
+    }
+}
