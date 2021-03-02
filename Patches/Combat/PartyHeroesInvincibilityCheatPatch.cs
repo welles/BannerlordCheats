eb585b7f9529525eac8b017f91dc808da7807e43
Adam Patterson <rueven54@hotmail.com>
2021-03-02 16:31:23 -0600

    * Added new version of invincible cheat that only affects player party heroes * Made it so that invincible party cheats affect horses, too

--- Patches/Combat/PartyHeroesInvincibilityCheatPatch.cs
+++ Patches/Combat/PartyHeroesInvincibilityCheatPatch.cs
@@ -6,13 +6,13 @@ using TaleWorlds.MountAndBlade;
 namespace BannerlordCheats.Patches
 {
     [HarmonyPatch(typeof(Agent), nameof(Agent.Invulnerable), MethodType.Getter)]
-    public static class PartyInvincibilityCheatPatch
+    public static class PartyHeroesInvincibilityCheatPatch
     {
         [HarmonyPostfix]
         public static void Invulnerable(ref Agent __instance, ref bool __result)
         {
-            if (!BannerlordCheatsSettings.Instance.PartyInvincible) return;
-            if (__instance.TryGetHuman(out var agent) && agent.Team?.IsPlayerTeam == true)
+            if (!BannerlordCheatsSettings.Instance.PartyHeroesInvincible) return;
+            if (__instance.TryGetHuman(out var agent) && agent.IsHero && agent.Team?.IsPlayerTeam == true)
             {
                 __result = true;
                 return;
