9ee9fbd70ca16a62c7af6b28e779fb48b6e848b2
Nico Welles <nico@welles.io>
2021-03-17 21:35:45 +0100

    Add Party extensions

---
+++ Extensions/PartyExtensions.cs
@@ -0,0 +1,12 @@
+﻿using TaleWorlds.CampaignSystem;
+
+namespace BannerlordCheats.Extensions
+{
+    public static class PartyExtensions
+    {
+        public static bool IsPlayerParty(this PartyBase party)
+        {
+            return party?.Leader?.IsPlayerCharacter ?? false;
+        }
+    }
+}
