f4ab17465ede5f3dd624371768a374e4d8d31587
Nico Welles <nico@welles.io>
2021-03-21 14:07:55 +0100

    Add Settlement extensions

---
+++ Extensions/SettlementExtensions.cs
@@ -0,0 +1,17 @@
+﻿using TaleWorlds.CampaignSystem;
+
+namespace BannerlordCheats.Extensions
+{
+    public static class SettlementExtensions
+    {
+        public static bool IsPlayerTown(this Town town)
+        {
+            return town?.Owner?.Owner?.IsHumanPlayerCharacter ?? false;
+        }
+
+        public static bool IsPlayerVillage(this Village village)
+        {
+            return village?.Owner?.Owner?.IsHumanPlayerCharacter ?? false;
+        }
+    }
+}
