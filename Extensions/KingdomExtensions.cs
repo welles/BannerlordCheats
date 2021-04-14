44de2de9aebb6f77bce878ef66d85d46b5beb964
Nico Welles <nico@welles.io>
2021-04-14 23:29:13 +0200

    Add Kingdom extensions

--- Extensions/KingdomExtensions.cs
+++ Extensions/KingdomExtensions.cs
@@ -5,9 +5,19 @@ namespace BannerlordCheats.Extensions
 {
     public static class KingdomExtensions
     {
+        public static bool IsPlayerKingdom(this Clan clan)
+        {
+            return clan?.Kingdom?.IsPlayerKingdom() ?? false;
+        }
+
         public static bool IsPlayerKingdom(this Kingdom kingdom)
         {
             return kingdom?.Clans?.Any(x => x.IsPlayerClan()) ?? false;
+        }
+
+        public static bool IsPlayerKingdom(this PartyBase party)
+        {
+            return party?.Owner?.Clan?.IsPlayerKingdom() ?? false;
         }
     }
 }
