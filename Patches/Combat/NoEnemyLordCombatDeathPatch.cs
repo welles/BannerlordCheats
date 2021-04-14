c63c16623285d2d83f2d7aaf820265dc8327bcda
Nico Welles <nico@welles.io>
2021-04-14 23:29:33 +0200

    Enable No Lord Death cheats in simulated battles

--- Patches/Combat/NoEnemyLordCombatDeathPatch.cs
+++ Patches/Combat/NoEnemyLordCombatDeathPatch.cs
@@ -1,7 +1,10 @@
-﻿using BannerlordCheats.Extensions;
+﻿using System;
+using BannerlordCheats.Extensions;
 using BannerlordCheats.Settings;
 using HarmonyLib;
 using SandBox;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
 using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
 
@@ -20,6 +23,23 @@ namespace BannerlordCheats.Patches.Combat
                 var factor = enemyLordCombatDeathPercentage / 100f;
 
                 __result *= factor;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(DefaultCombatSimulationModel), nameof(DefaultCombatSimulationModel.SimulateHit))]
+    public static class NoEnemyLordCombatDeathSimulationPatch
+    {
+        [HarmonyPostfix]
+        public static void SimulateHit(ref CharacterObject strikerTroop, ref CharacterObject struckTroop, ref PartyBase strikerParty, ref PartyBase struckParty, ref float strikerAdvantage, ref MapEvent battle, ref int __result)
+        {
+            if (struckTroop.IsHero()
+                && !struckParty.IsPlayerKingdom()
+                && BannerlordCheatsSettings.TryGetModifiedValue(x => x.EnemyLordCombatDeathPercentage, out var enemyLordCombatDeathPercentage))
+            {
+                var factor = enemyLordCombatDeathPercentage / 100f;
+
+                __result = (int) Math.Round(__result / factor);
             }
         }
     }
