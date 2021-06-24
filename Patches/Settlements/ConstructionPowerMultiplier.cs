bfa3469c7cc5c21e0fac5b8986e455271ce8adbd
Nico Welles <nico@welles.io>
2021-06-24 14:24:26 +0200

    Disable Construction Power Multiplier cheat for beta

--- Patches/Settlements/ConstructionPowerMultiplier.cs
+++ Patches/Settlements/ConstructionPowerMultiplier.cs
@@ -6,20 +6,20 @@ using TaleWorlds.CampaignSystem.SandBox.GameComponents;
 
 namespace BannerlordCheats.Patches.Settlements
 {
-    [HarmonyPatch(typeof(DefaultBuildingConstructionModel), nameof(DefaultBuildingConstructionModel.CalculateDailyConstructionPower))]
-    public static class ConstructionPowerMultiplier
-    {
-        [HarmonyPostfix]
-        public static void CalculateDailyConstructionPower(
-            ref Town town,
-            ref bool includeDescriptions,
-            ref ExplainedNumber __result)
-        {
-            if (town.IsPlayerTown()
-                && BannerlordCheatsSettings.TryGetModifiedValue(settings => settings.ConstructionPowerMultiplier, out var constructionPowerMultiplier))
-            {
-                __result.AddMultiplier(constructionPowerMultiplier);
-            }
-        }
-    }
+    // [HarmonyPatch(typeof(DefaultBuildingConstructionModel), nameof(DefaultBuildingConstructionModel.CalculateDailyConstructionPower))]
+    // public static class ConstructionPowerMultiplier
+    // {
+    //     [HarmonyPostfix]
+    //     public static void CalculateDailyConstructionPower(
+    //         ref Town town,
+    //         ref bool includeDescriptions,
+    //         ref ExplainedNumber __result)
+    //     {
+    //         if (town.IsPlayerTown()
+    //             && BannerlordCheatsSettings.TryGetModifiedValue(settings => settings.ConstructionPowerMultiplier, out var constructionPowerMultiplier))
+    //         {
+    //             __result.AddMultiplier(constructionPowerMultiplier);
+    //         }
+    //     }
+    // }
 }
