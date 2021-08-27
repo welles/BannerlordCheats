43bb77abd0a071e3dd7665191b9147ade0b6ad0b
Nico Welles <nico@welles.io>
2021-08-27 18:44:45 +0200

    Disable Free Troop Recruitment cheat

--- Patches/Settlements/FreeTroopRecruitment.cs
+++ Patches/Settlements/FreeTroopRecruitment.cs
@@ -6,17 +6,17 @@ using TaleWorlds.CampaignSystem.SandBox.GameComponents.Party;
 
 namespace BannerlordCheats.Patches.Settlements
 {
-    [HarmonyPatch(typeof(DefaultPartyWageModel), nameof(DefaultPartyWageModel.GetTroopRecruitmentCost))]
-    public static class FreeTroopRecruitment
-    {
-        [HarmonyPostfix]
-        public static void GetTroopRecruitmentCost(CharacterObject troop, Hero buyerHero, bool withoutItemCost, ref int __result)
-        {
-            if (buyerHero.IsPlayer()
-                && BannerlordCheatsSettings.Instance?.FreeTroopRecruitment == true)
-            {
-                __result = 1;
-            }
-        }
-    }
+    // [HarmonyPatch(typeof(DefaultPartyWageModel), nameof(DefaultPartyWageModel.GetTroopRecruitmentCost))]
+    // public static class FreeTroopRecruitment
+    // {
+    //     [HarmonyPostfix]
+    //     public static void GetTroopRecruitmentCost(CharacterObject troop, Hero buyerHero, bool withoutItemCost, ref int __result)
+    //     {
+    //         if (buyerHero.IsPlayer()
+    //             && BannerlordCheatsSettings.Instance?.FreeTroopRecruitment == true)
+    //         {
+    //             __result = 1;
+    //         }
+    //     }
+    // }
 }
