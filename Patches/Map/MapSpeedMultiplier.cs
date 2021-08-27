d31d97fef3a88e585406f8f31386607056980cb2
Nico Welles <nico@welles.io>
2021-08-27 18:48:37 +0200

    Disable Map Speed Multiplier cheat

--- Patches/Map/MapSpeedMultiplier.cs
+++ Patches/Map/MapSpeedMultiplier.cs
@@ -6,17 +6,17 @@ using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
 
 namespace BannerlordCheats.Patches.Map
 {
-    [HarmonyPatch(typeof(DefaultPartySpeedCalculatingModel), nameof(DefaultPartySpeedCalculatingModel.CalculateFinalSpeed))]
-    public static class MapSpeedMultiplier
-    {
-        [HarmonyPostfix]
-        public static void CalculateFinalSpeed(ref MobileParty mobileParty, ref ExplainedNumber finalSpeed, ref ExplainedNumber __result)
-        {
-            if (mobileParty.IsPlayerParty()
-                && BannerlordCheatsSettings.Instance?.MapSpeedMultiplier > 1f)
-            {
-                __result.AddMultiplier(BannerlordCheatsSettings.Instance.MapSpeedMultiplier);
-            }
-        }
-    }
+    // [HarmonyPatch(typeof(DefaultPartySpeedCalculatingModel), nameof(DefaultPartySpeedCalculatingModel.CalculateFinalSpeed))]
+    // public static class MapSpeedMultiplier
+    // {
+    //     [HarmonyPostfix]
+    //     public static void CalculateFinalSpeed(ref MobileParty mobileParty, ref ExplainedNumber finalSpeed, ref ExplainedNumber __result)
+    //     {
+    //         if (mobileParty.IsPlayerParty()
+    //             && BannerlordCheatsSettings.Instance?.MapSpeedMultiplier > 1f)
+    //         {
+    //             __result.AddMultiplier(BannerlordCheatsSettings.Instance.MapSpeedMultiplier);
+    //         }
+    //     }
+    // }
 }
