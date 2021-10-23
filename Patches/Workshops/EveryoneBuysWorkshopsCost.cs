29fab9adab3322aaa42c015812722582b13632b0
Nico Welles <nico@welles.io>
2021-10-23 12:10:19 +0200

    Add Everyone Buys Workshops cheat

---
+++ Patches/Workshops/EveryoneBuysWorkshopsCost.cs
@@ -0,0 +1,33 @@
+﻿using BannerlordCheats.Extensions;
+using BannerlordCheats.Settings;
+using HarmonyLib;
+using JetBrains.Annotations;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.Localization;
+
+namespace BannerlordCheats.Patches.Workshops
+{
+    [HarmonyPatch(typeof(ChangeOwnerOfWorkshopAction), nameof(ChangeOwnerOfWorkshopAction.ApplyByTrade))]
+    public static class EveryoneBuysWorkshopsCost
+    {
+        [UsedImplicitly]
+        [HarmonyPrefix]
+        public static void ApplyByTrade(
+            ref Workshop workshop,
+            ref Hero newOwner,
+            ref WorkshopType workshopType,
+            ref int capital,
+            ref bool upgradable,
+            ref int cost,
+            ref TextObject customName)
+        {
+            if (BannerlordCheatsSettings.Instance?.EveryoneBuysWorkshops == true
+                && workshop.Owner.IsPlayer()
+                && cost > newOwner.Gold)
+            {
+                newOwner.Gold = cost;
+            }
+        }
+    }
+}
