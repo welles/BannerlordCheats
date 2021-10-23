a1fe247352d8779ae9ec9d9b27474f9e089159d6
Nico Welles <nico@welles.io>
2021-10-23 14:46:19 +0200

    Show warning message when using mod for the first time

--- MBInitialScreenBasePatch.cs
+++ MBInitialScreenBasePatch.cs
@@ -1,4 +1,7 @@
-﻿using HarmonyLib;
+﻿using System.IO;
+using System.Reflection;
+using BannerlordCheats.Localization;
+using HarmonyLib;
 using JetBrains.Annotations;
 using TaleWorlds.Core;
 using TaleWorlds.Library;
@@ -19,6 +22,55 @@ namespace BannerlordCheats
                 InformationManager.DisplayMessage(new InformationMessage($"Detailed error info has been saved in the file {SubModule.ErrorFile} in the Cheats mod folder.", Color.FromUint(16711680)));
                 InformationManager.DisplayMessage(new InformationMessage("If possible please send this file to the mod author.", Color.FromUint(16711680)));
                 InformationManager.DisplayMessage(new InformationMessage("You can still play the game, but some cheats may not work.", Color.FromUint(16711680)));
+            }
+
+            if (!ConfirmFileExists())
+            {
+                InformationManager.ShowInquiry(new InquiryData(
+                    L10N.GetText("ModName"),
+                    L10N.GetText("ModWarningMessage"),
+                    true,
+                    false,
+                    L10N.GetText("ModWarningMessageConfirm"),
+                    null,
+                    CreateConfirmFile,
+                    null));
+            }
+        }
+
+        private static void CreateConfirmFile()
+        {
+            try
+            {
+                var confirmFileName = $"I Understand How The Cheats Work.txt";
+
+                var location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+                var confirmFilePath = Path.Combine(location, confirmFileName);
+
+                File.Create(confirmFileName);
+            }
+            catch
+            {
+                // Not worth crashing for
+            }
+        }
+
+        private static bool ConfirmFileExists()
+        {
+            try
+            {
+                var confirmFileName = $"I Understand How The Cheats Work.txt";
+
+                var location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+                var confirmFilePath = Path.Combine(location, confirmFileName);
+
+                return File.Exists(confirmFilePath);
+            }
+            catch
+            {
+                return false;
             }
         }
     }
