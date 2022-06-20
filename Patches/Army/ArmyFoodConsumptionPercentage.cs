f4611baeb71e5d84e1c836e277f7adacd0866cf2
Nico Welles <nico@welles.io>
2022-06-20 12:21:54 +0200

    Fix food consumption cheats for beta

--- Patches/Army/ArmyFoodConsumptionPercentage.cs
+++ Patches/Army/ArmyFoodConsumptionPercentage.cs
@@ -14,7 +14,10 @@ namespace BannerlordCheats.Patches.Army
     {
         [UsedImplicitly]
         [HarmonyPostfix]
-        public static void CalculateDailyFoodConsumptionf(ref MobileParty party, ref bool includeDescription, ref ExplainedNumber __result)
+        public static void CalculateDailyFoodConsumptionf(
+            ref MobileParty party,
+            ref ExplainedNumber baseConsumption,
+            ref ExplainedNumber __result)
         {
             try
             {
