5bd7a4d45a38eb3897a2ece4baff156fb1211537
Nico Welles <nico@welles.io>
2022-09-12 23:21:18 +0200

    Update Always Knock Down for new version

--- Patches/Combat/AlwaysKnockDown.cs
+++ Patches/Combat/AlwaysKnockDown.cs
@@ -3,27 +3,27 @@ using BannerlordCheats.Extensions;
 using BannerlordCheats.Settings;
 using HarmonyLib;
 using JetBrains.Annotations;
+using SandBox.GameComponents;
 using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
 
 namespace BannerlordCheats.Patches.Combat
 {
-    [HarmonyPatch(typeof(Mission), "DecideAgentKnockedByBlow")]
+    [HarmonyPatch(typeof(SandboxAgentApplyDamageModel), nameof(SandboxAgentApplyDamageModel.DecideAgentKnockedDownByBlow))]
     public static class AlwaysKnockDown
     {
         [UsedImplicitly]
         [HarmonyPostfix]
         public static void DecideAgentKnockedByBlow(
-            ref Agent attacker,
-            ref Agent victim,
+            ref Agent attackerAgent,
+            ref Agent victimAgent,
             ref AttackCollisionData collisionData,
             ref WeaponComponentData attackerWeapon,
-            ref bool isInitialBlowShrugOff,
             ref Blow blow)
         {
             try
             {
-                if (attacker.IsPlayer()
+                if (attackerAgent.IsPlayer()
                     && BannerlordCheatsSettings.Instance?.AlwaysKnockDown == true)
                 {
                     blow.BlowFlag &= ~BlowFlags.ShrugOff;
