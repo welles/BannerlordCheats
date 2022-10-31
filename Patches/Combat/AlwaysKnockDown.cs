7bab6936afce149cfd97f217075ba3f2c499eaa2
Nico Welles <nico@welles.io>
2022-10-31 14:33:30 +0100

    Split Always Knock Down for modules

--- Patches/Combat/AlwaysKnockDown.cs
+++ Patches/Combat/AlwaysKnockDown.cs
@@ -9,12 +9,8 @@ using TaleWorlds.MountAndBlade;
 
 namespace BannerlordCheats.Patches.Combat
 {
-    [HarmonyPatch(typeof(DefaultAgentApplyDamageModel), nameof(DefaultAgentApplyDamageModel.DecideAgentKnockedDownByBlow))]
-    [HarmonyPatch(typeof(SandboxAgentApplyDamageModel), nameof(SandboxAgentApplyDamageModel.DecideAgentKnockedDownByBlow))]
     public static class AlwaysKnockDown
     {
-        [UsedImplicitly]
-        [HarmonyPostfix]
         public static void DecideAgentKnockedByBlow(
             ref Agent attackerAgent,
             ref Agent victimAgent,
@@ -36,5 +32,43 @@ namespace BannerlordCheats.Patches.Combat
                 SubModule.LogError(e, typeof(AlwaysKnockDown));
             }
         }
+    }
+
+    [HarmonyPatch(typeof(DefaultAgentApplyDamageModel), nameof(DefaultAgentApplyDamageModel.DecideAgentKnockedDownByBlow))]
+    public static class AlwaysKnockDown_Default
+    {
+        [UsedImplicitly]
+        [HarmonyPostfix]
+        public static void DecideAgentKnockedByBlow(
+            ref Agent attackerAgent,
+            ref Agent victimAgent,
+            ref AttackCollisionData collisionData,
+            ref WeaponComponentData attackerWeapon,
+            ref Blow blow)
+            => AlwaysKnockDown.DecideAgentKnockedByBlow(
+                ref attackerAgent,
+                ref victimAgent,
+                ref collisionData,
+                ref attackerWeapon,
+                ref blow);
+    }
+
+    [HarmonyPatch(typeof(SandboxAgentApplyDamageModel), nameof(SandboxAgentApplyDamageModel.DecideAgentKnockedDownByBlow))]
+    public static class AlwaysKnockDown_Sandbox
+    {
+        [UsedImplicitly]
+        [HarmonyPostfix]
+        public static void DecideAgentKnockedByBlow(
+            ref Agent attackerAgent,
+            ref Agent victimAgent,
+            ref AttackCollisionData collisionData,
+            ref WeaponComponentData attackerWeapon,
+            ref Blow blow)
+            => AlwaysKnockDown.DecideAgentKnockedByBlow(
+                ref attackerAgent,
+                ref victimAgent,
+                ref collisionData,
+                ref attackerWeapon,
+                ref blow);
     }
 }
