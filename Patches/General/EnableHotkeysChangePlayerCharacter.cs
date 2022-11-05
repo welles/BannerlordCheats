b6c5b562a5fe7e3123d4a22abbd6ab5cd276cff8
Nico Welles <nico@welles.io>
2022-11-05 15:33:21 +0100

    Add Change Player Character hotkey

---
+++ Patches/General/EnableHotkeysChangePlayerCharacter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using BannerlordCheats.Extensions;
+using BannerlordCheats.Localization;
+using BannerlordCheats.Settings;
+using HarmonyLib;
+using JetBrains.Annotations;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Encyclopedia.Pages;
+using TaleWorlds.InputSystem;
+using TaleWorlds.Library;
+
+namespace BannerlordCheats.Patches.General
+{
+    [HarmonyPatch(typeof(EncyclopediaPageVM), "OnTick")]
+    public static class EnableHotkeysChangePlayerCharacter
+    {
+        [UsedImplicitly]
+        [HarmonyPostfix]
+        public static void OnTick(ref EncyclopediaPageVM __instance)
+        {
+            try
+            {
+                if (__instance is EncyclopediaHeroPageVM
+                    && __instance.Obj is Hero hero
+                    && BannerlordCheatsSettings.Instance?.EnableHotkeys == true)
+                {
+                    if (Keys.IsKeyPressed(InputKey.H, InputKey.LeftControl))
+                    {
+                        InformationManager.ShowInquiry(
+                            new InquiryData(L10N.GetTextFormat("ChangePlayerMessageTitle", hero.Name),
+                                L10N.GetText("ChangePlayerMessage"), true, true,
+                                L10N.GetText("ChangePlayerConfirm"), L10N.GetText("Cancel"),
+                                () => ChangePlayerCharacterAction.Apply(hero), null), true, true);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                SubModule.LogError(e, typeof(EnableHotkeysKillCharacter));
+            }
+        }
+    }
+}
