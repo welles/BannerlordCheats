688df9c7458a64364fe4357ee18c49afdc116a0d
Nico Welles <nico@welles.io>
2023-03-12 16:36:40 +0100

    Remove usages of DefaultAgentApplyDamageModel

--- Patches/Combat/AlwaysKnockDown.cs
+++ Patches/Combat/AlwaysKnockDown.cs
@@ -34,25 +34,6 @@ namespace BannerlordCheats.Patches.Combat
         }
     }
 
-    [HarmonyPatch(typeof(DefaultAgentApplyDamageModel), nameof(DefaultAgentApplyDamageModel.DecideAgentKnockedDownByBlow))]
-    public static class AlwaysKnockDown_Default
-    {
-        [UsedImplicitly]
-        [HarmonyPostfix]
-        public static void DecideAgentKnockedByBlow(
-            ref Agent attackerAgent,
-            ref Agent victimAgent,
-            ref AttackCollisionData collisionData,
-            ref WeaponComponentData attackerWeapon,
-            ref Blow blow)
-            => AlwaysKnockDown.DecideAgentKnockedByBlow(
-                ref attackerAgent,
-                ref victimAgent,
-                ref collisionData,
-                ref attackerWeapon,
-                ref blow);
-    }
-
     [HarmonyPatch(typeof(SandboxAgentApplyDamageModel), nameof(SandboxAgentApplyDamageModel.DecideAgentKnockedDownByBlow))]
     public static class AlwaysKnockDown_Sandbox
     {
--- Patches/Combat/NeverKnockedBackByAttacks.cs
+++ Patches/Combat/NeverKnockedBackByAttacks.cs
@@ -35,25 +35,6 @@ namespace BannerlordCheats.Patches.Combat
         }
     }
 
-    [HarmonyPatch(typeof(DefaultAgentApplyDamageModel), nameof(DefaultAgentApplyDamageModel.DecideAgentKnockedBackByBlow))]
-    public static class NeverKnockedBackByAttacks_Default
-    {
-        [UsedImplicitly]
-        [HarmonyPostfix]
-        public static void DecideAgentKnockedByBlow(
-            ref Agent attackerAgent,
-            ref Agent victimAgent,
-            ref AttackCollisionData collisionData,
-            ref WeaponComponentData attackerWeapon,
-            ref Blow blow)
-            => NeverKnockedBackByAttacks.DecideAgentKnockedByBlow(
-                ref attackerAgent,
-                ref victimAgent,
-                ref collisionData,
-                ref attackerWeapon,
-                ref blow);
-    }
-
     [HarmonyPatch(typeof(SandboxAgentApplyDamageModel), nameof(SandboxAgentApplyDamageModel.DecideAgentKnockedBackByBlow))]
     public static class NeverKnockedBackByAttacks_Sandbox
     {
